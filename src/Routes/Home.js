e4f76538f515619a5e47e32397c77add1376d6a7
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-01 23:10:35 -0300

    implementa layout telas home e register

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,5 +1,78 @@
+import styled from "styled-components";
+
 export default function Home() {
     return (
-        <></>
+        <Container>
+            <header>
+                <h2>Olá, Fulano</h2>
+                <ion-icon name="exit-outline"></ion-icon>
+            </header>
+            <main>
+                <p>Não há registros de</p>
+                <p>entrada ou saída</p>
+            </main>
+            <footer>
+                <div>
+                    <ion-icon name="add-circle-outline"></ion-icon>
+                    <p>Nova <br/> entrada</p>
+                </div>
+                <div>
+                    <ion-icon name="remove-circle-outline"></ion-icon>
+                    <p>Nova <br/> saída</p>
+                </div>
+            </footer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 15px 25px;
+
+header {
+    display: flex;
+    justify-content: space-between;
+    font-size: 30px;
+    font-weight: 700;
+    width: 330px;
+}
+
+main {
+    min-height: 500px;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    color: #868686;
+    width: 330px;
+}
+
+footer {
+    display: flex;
+    justify-content: space-between;
+    width: 330px;
+    font-size: 30px;
+    font-weight: 700;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: #A328D6;
+        width: 155px;
+        height: 114px;
+        border-radius: 5px;
+        padding: 10px;
+
+        p {
+            font-size: 20px;
+        }
+    }
+}
+`
--- src/Routes/Register.js
+++ src/Routes/Register.js
@@ -1,5 +1,61 @@
+import styled from "styled-components"
+
 export default function Register() {
     return (
-        <></>
+        <Container>
+            <header>Nova entrada</header>
+            <form>
+                <input placeholder="Valor" />
+                <input placeholder="Descrição" />
+                <button>Salvar entrada</button>
+            </form>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 25px;
+
+header {
+    width: 100%;
+    max-width: 1000px;
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 40px;
+}
+
+form {
+    width: 100%;
+    max-width: 1000px;
+    display: flex;
+    flex-direction: column;
+}
+
+input {
+    height: 58px;
+    margin-bottom: 13px;
+    font-size: 20px;
+    padding-left: 15px;
+    border-radius: 5px;
+    border: none;
+}
+
+input::placeholder {
+    color: #000000;
+    opacity: 1;
+}
+
+button {
+    height: 46px;
+    background-color: #A328D6;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+}
+`
